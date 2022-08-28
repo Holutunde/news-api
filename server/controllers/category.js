acd8ddaed57b0367360912efad3b7543de741fd8
Holutunde <Holutunde99@gmail.com>
2022-08-28 23:47:49 +0100

    delete category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -20,6 +20,23 @@ const addCategory = async (req, res) => {
   })
 }
 
+const deleteCategory = async (req, res) => {
+  const id = req.params.id
+  const category = await Category.findByIdAndDelete(id)
+
+  if (!category) {
+    return res.status(401).json({
+      success: false,
+      msg: 'Category not found.',
+    })
+  }
+  res.status(201).json({
+    success: true,
+    msg: 'Successfully Deleted',
+    data: category,
+  })
+}
+
 module.exports = {
   addCategory,
   deleteCategory,
